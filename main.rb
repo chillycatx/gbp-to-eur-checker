7de1fd1149a5556f48874b49122e7c1acc266020
Stina <55795211+chillycatx@users.noreply.github.com>
2019-10-07 22:32:31 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,1 +1,15 @@
 #!/usr/bin/env ruby
+require 'net/https'
+require 'uri'
+
+url = 'http://data.fixer.io/api/latest'
+uri = URI(url)
+
+Net::HTTP.start(uri.host, uri.port,
+    :use_ssl => uri.scheme == 'https', 
+    :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
+
+    request = Net::HTTP::Get.new uri.request_uri
+    response = http.request request
+    puts response.body
+end
